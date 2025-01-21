3dadcad77391f4997e8675358d74b735bea066e6
Muhammad Taha Sami <99526410+itzztahahere@users.noreply.github.com>
2025-01-21 21:33:16 +0500

    Update useCategory.js

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import apiUrl from '../utils/config'
 
 const useCategory = () => {
     const [categories, setCategories] = useState([]);
 
     const fetchCategories = async () => {
         try {
-            const { data } = await axios.get('${apiUrl}/get-categories');
+            const { data } = await axios.get(`${apiUrl}/get-categories`);
             setCategories(data?.categories || []);
         } catch (error) {
             console.error('Error fetching categories:', error);
